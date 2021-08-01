6fe2174e154731355d940a316ecbcd780ad4716d
hrk0930 <a08062151874@icloud.com>
2021-08-01 12:08:27 +0900

    編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
    with_options presence: true do
     # presence:trueのみ適用されるカラムを書き出す
     
-      with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
-      validates :first_name
+      with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :first_name  
       validates :last_name
       end 
 
@@ -18,23 +18,12 @@ class User < ApplicationRecord
      validates :last_name_kana
      end 
     
-     validates :first_name_kana,    format: { with: /\A[ァ-ヶー－]+\z/ }
-     validates :last_name_kana,     format: { with: /\A[ァ-ヶー－]+\z/ }
      validates :nick_name
      validates :birth,              format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
      end
 
    validates :password,           format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i }
 
-
-
-  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :nick_name,          presence: true
-  validates :password,           format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i }
-  validates :birth,              presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/ }
 
   has_many :items
   has_many :buy_managements
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
 
-
-
   devise_for :users
   #resources  :users,    only: :show
-
-
-  devise_for :users
-  #resources  :users,    only: :show
-
-
-  devise_for :users
-
 
   root to: 'items#index'
   resources  :items,    only: [:index, :new]
