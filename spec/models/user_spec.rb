2662f99b9aa30daed92566952a1a1b62ba93a7b6
hrk0930 <a08062151874@icloud.com>
2021-08-01 15:17:03 +0900

    編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,12 +11,7 @@ RSpec.describe User, type: :model do
           email、password、password_confirmation、birthdayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
-
-      it 'passwordが6文字以上であれば登録できる' do
-        @user.password = 'aaaaa1'
-        @user.password_confirmation = 'aaaaa1'
-        expect(@user).to be_valid
-      end
+      
     end
 
     context '新規登録できないとき' do
