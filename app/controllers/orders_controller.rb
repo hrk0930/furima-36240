c4fa9940a93d4fb18207b0a22c43343792bb4651
hrk0930 <a08062151874@icloud.com>
2021-08-07 09:06:11 +0900

    クレジット決済

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,8 +8,9 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+       pay_item
        @order_address.save
-       redirect_to root_path
+       return redirect_to root_path
     else
        render :index
     end
@@ -20,7 +21,16 @@ class OrdersController < ApplicationController
    private
 
       def order_params
-        params.require(:order_address).permit(:post_number, :city, :address, :building, :telephone_number, :outgoing_area_id).merge(user_id: current_user.id, item_id: params[:item_id])
+        params.require(:order_address).permit(:post_number, :city, :address, :building, :telephone_number, :outgoing_area_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+      end
+
+      def pay_item      
+         Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+             Payjp::Charge.create(
+               amount: @item.price,  # 商品の値段
+               card: order_params[:token],    # カードトークン
+               currency: 'jpy'                # 通貨の種類（日本円）
+             )
       end
 
 end
--- spec/factories/orders_addresses.rb
+++ spec/factories/orders_addresses.rb
@@ -5,9 +5,19 @@ FactoryBot.define do
     address             {"港区"}
     telephone_number    {"08011111111"}
     outgoing_area_id    {2}
+    price {3000}
+    token               {"tok_abcdefghijk00000000000000000"}
 
   end
 
+  #it "priceとtokenがあれば保存ができること" do
+   # expect(@order).to be_valid
+  #end
+  #it "tokenが空では登録できないこと" do
+   # @order.token = nil
+   # @order.valid?
+  #  expect(@order.errors.full_messages).to include("Token can't be blank")
+  #end
 
 
 
