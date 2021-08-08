5dc093b0c0dc6262ad15d724fe67fd700ea3417a
hrk0930 <a08062151874@icloud.com>
2021-08-08 14:25:07 +0900

    編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, only: [:index]
   before_action :set_item,  only: [:index]
   before_action :user_redirect, only: [:index]
 
@@ -33,24 +33,21 @@ class OrdersController < ApplicationController
       def pay_item      
          Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
              Payjp::Charge.create(
-               amount: @item.price,  # 商品の値段
+               amount: @item.price,           # 商品の値段
                card: order_params[:token],    # カードトークン
                currency: 'jpy'                # 通貨の種類（日本円）
              )
       end
-      
-    def user_redirect
-      if user_signed_in? && current_user.id == @item.user_id || @item.order.present?
-        redirect_to root_path
+      #出品者が購入ページに遷移した時、または売れた商品の購入ページに遷移した時はroot＿pathに遷移される
+      def user_redirect
+        redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
       end
+      #ユーザーがログインしていない場合ログインページに遷移する
        unless user_signed_in? 
         redirect_to new_user_session_path
        end
     end
-
     def set_item
       @item = Item.find(params[:item_id])
     end
-
-
 end
