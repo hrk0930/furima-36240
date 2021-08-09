45a9e9283b39ecf02cdaaf449dd533e7e627c68e
hrk0930 <a08062151874@icloud.com>
2021-08-09 16:57:44 +0900

    編集

--- app/models/status.rb
+++ app/models/status.rb
@@ -5,7 +5,7 @@ class Status < ActiveHash::Base
     { id: 2, name: '新品・未使用' },
     { id: 3, name: '未使用に近い' },
     { id: 4, name: '目立った傷や汚れなし' },
-    { id: 5, name: '傷や汚れなし' },
+    { id: 5, name: '傷や汚れあり' },
     { id: 6, name: '全体的に状態が悪い' }
   ]
  
